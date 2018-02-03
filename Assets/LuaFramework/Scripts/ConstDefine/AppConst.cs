01a34e606673d1d173a288087bf3245e0ca63bf4
lxt <luo777xt@sina.com>
2018-02-03 17:06:31 +0800

    将AppConst的名字改回来
    发现修改这个关键字partial可能会产生一些不利的影响。

--- Assets/LuaFramework/Scripts/ConstDefine/AppConst.cs
+++ Assets/LuaFramework/Scripts/ConstDefine/AppConst.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace LuaFramework {
-        public partial class AppConst {
+        public class AppConst {
         public const bool DebugMode = false;                       //调试模式-用于内部测试
         /// <summary>
         /// 如果想删掉框架自带的例子，那这个例子模式必须要
@@ -18,26 +18,26 @@ namespace LuaFramework {
         /// 下面是打包配置
         /// </summary>
 
-        public const bool UpdateMode = true;                       //更新模式-默认关闭 
-        public const bool LuaByteMode = false;                     //Lua字节码模式-默认关闭 
-        public const bool LuaBundleMode = true;                    //Lua代码AssetBundle模式
+        public const bool UpdateMode = false;                      //更新模式-默认关闭 
+        public const bool LuaByteMode = false;                    //Lua字节码模式-默认关闭 
+        public const bool LuaBundleMode = false;                   //Lua代码AssetBundle模式
         /// <summary>
         /// 调试配置
         /// </summary>
-        //public const bool UpdateMode = false;                       //更新模式-默认关闭 
-        //public const bool LuaByteMode = false;                      //Lua字节码模式-默认关闭 
-        //public const bool LuaBundleMode = false;                    //Lua代码AssetBundle模式
+        //public const bool UpdateMode = false;                     //更新模式-默认关闭 
+        //public const bool LuaByteMode = false;                    //Lua字节码模式-默认关闭 
+        //public const bool LuaBundleMode = false;                  //Lua代码AssetBundle模式
 
         public const int TimerInterval = 1;
-        public const int GameFrameRate = 30;                        //游戏帧频
+        public const int GameFrameRate = 40;                        //游戏帧频
 
         public const string AppName = "LuaFramework";//"LuaRaz";    //应用程序名称
         public const string LuaTempDir = "LuaTemp/";                //临时目录
         public const string AppPrefix = AppName + "_";              //应用程序前缀
         public const string ExtName = ".unity3d";                   //素材扩展名
         public const string AssetDir = "StreamingAssets";           //素材目录 
-        public const string WebUrl = "https://www.dsz0.com/cnt/StreamingAssets/";      //测试更新地址
-
+        //public const string WebUrl = "https://www.dsz0.com/cnt/StreamingAssets/";      //测试更新地址
+        public const string WebUrl = "https://www.dsz0.com/and/luaframework/";      //测试更新地址
         public static string UserId = string.Empty;                 //用户ID
         public static int SocketPort = 0;                           //Socket服务器端口
         public static string SocketAddress = string.Empty;          //Socket服务器地址
